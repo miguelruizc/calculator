9387e0e744bdd966eaab3aa0d3b2864ba213bdd9
miguelruizc <buumchakalaka@gmail.com>
2024-03-25 17:23:13 +0100

    Add logic to show displayValue in screen

--- script.js
+++ script.js
@@ -54,38 +54,38 @@ function handleClick(event){
             console.log("div clicked");
             break; 
         case "digit0":
-            displayValue += "0";
+            (displayValue == "0") ? (displayValue = "0") : (displayValue += "0");
             break;
         case "digit1":
-            displayValue += "1";
+            (displayValue == "0") ? (displayValue = "1") : (displayValue += "1");
             break; 
         case "digit2":
-            displayValue += "2";
+            (displayValue == "0") ? (displayValue = "2") : (displayValue += "2");
             break;
         case "digit3":
-            displayValue += "3";
+            (displayValue == "0") ? (displayValue = "3") : (displayValue += "3");
             break; 
         case "digit4":
-            displayValue += "4";
+            (displayValue == "0") ? (displayValue = "4") : (displayValue += "4");
             break;
         case "digit5":
-            displayValue += "5";
+            (displayValue == "0") ? (displayValue = "5") : (displayValue += "5");
             break;
         case "digit6":
-            displayValue += "6";
+            (displayValue == "0") ? (displayValue = "6") : (displayValue += "6");
             break;
         case "digit7":
-            displayValue += "7";
+            (displayValue == "0") ? (displayValue = "7") : (displayValue += "7");
             break; 
         case "digit8":
-            displayValue += "8";
+            (displayValue == "0") ? (displayValue = "8") : (displayValue += "8");
             break;
         case "digit9":
-            displayValue += "9";
+            (displayValue == "0") ? (displayValue = "9") : (displayValue += "9");
             break; 
     } 
 
-    console.log(displayValue);
+    updateDisplay();
 }
 
 function initialize(){
@@ -97,12 +97,17 @@ function initialize(){
     });
 }
 
-// Set event listeners
-initialize();
+function updateDisplay(){
+    const screen = document.querySelector(".screen");
+    screen.textContent = displayValue;
+}
 
-// operands a & b, and operator 
+// variables for app
 let a = 0;
 let b = 0;
 let operator = "";
-let displayValue = "";
+let displayValue = "0";
 
+// Initialize 
+initialize();
+updateDisplay();

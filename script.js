3cb7f17645e0defed232a295e1b0a626f28d6d69
miguelruizc <buumchakalaka@gmail.com>
2024-03-25 15:23:13 +0100

    Add operate() function

--- script.js
+++ script.js
@@ -14,6 +14,25 @@ function divide (a, b){
     return a / b;
 }
 
+function operate (a, b, operator){
+    switch (operator){
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+        default: 
+            return "Invalid operator";
+    }
+}
+
 // operands a & b, and operator 
 let a = 0;
 let b = 0;
